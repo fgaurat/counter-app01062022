79e0a0485d9a70c15314a6242dc504c97bfec538
Frédéric Gaurat <fgaurat@eolem.com>
2022-06-01 15:21:49 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, reset } from './features/counter/counterSlice';
+
 
 function App() {
+  
+  const count = useSelector((state) => state.counter.value)
+  const name = useSelector((state) => state.counter.name)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{name}</h2>
+        <button onClick={() => dispatch(increment())}>inc</button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+        <button onClick={() => dispatch(decrement())}>dec</button>
+        <br />
+        {count}
     </div>
   );
 }
